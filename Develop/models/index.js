297eb0eebe89caabebcfa42bf617ea2add785171
Jonathan Pohahau <j.pohahau@outlook.com>
2023-02-05 10:10:22 -0800

    added the init for the models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -4,13 +4,30 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Reader.hasOne(LibraryCard, {
+//   foreignKey: 'reader_id',
+//   onDelete: 'CASCADE',
+// });
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'reader_id',
+  onDelete: 'CASCADE',
+});
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'reader_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsTo(ProductTag, {
+  foreignKey: 'reader_id',
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsTo(ProductTag, {
+  foreignKey: 'reader_id',
+});
 
 module.exports = {
   Product,
