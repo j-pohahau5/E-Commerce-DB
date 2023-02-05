d933eb8e9de9e8bd108054cf18287d090e84191a
Jonathan Pohahau <j.pohahau@outlook.com>
2023-02-05 14:17:59 -0800

    fixeds const for product get rourtes

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -8,10 +8,10 @@ router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   try {
-    const category = await Product.findAll({
+    const product = await Product.findAll({
       include: [{ model: Category }, { model: Tag }],
     });
-    res.status(200).json(category);
+    res.status(200).json(product);
   } catch (err) {
     res.status(500).json(err);
   }
@@ -22,16 +22,16 @@ router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
   try {
-    const category = await Product.findByPk(req.params.id, {
+    const product = await Product.findByPk(req.params.id, {
       include: [{ model: Category }, { model: Tag }],
     });
 
-    if (!category) {
+    if (!product) {
       res.status(404).json({ message: 'No product found with that id!' });
       return;
     }
 
-    res.status(200).json(category);
+    res.status(200).json(product);
   } catch (err) {
     res.status(500).json(err);
   }
